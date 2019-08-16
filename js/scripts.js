2ad0e28fe72531598cc3f4ce3470a421b8623ba0
Chris Burge <topher.burge@gmail.com>
2019-08-16 14:27:36 -0700

    add name prompt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
+  var name = prompt("Please enter your name.")
   $(".survey").submit(function(event){
     $(".result").hide();
 
     event.preventDefault();
+
     var career = $("select#career").val();
     var company = $("select#company").val();
     var important = $("select#important").val();
     var challenge = $("select#challenge").val();
     var animal = $("select#animal").val();
-    var nameInput = $("input#name").val();
 
-    $(".name").text(nameInput);
+    $(".name").text(name);
 
     if (career === "dev") {
         $("#python").show();
