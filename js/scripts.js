f3367d873ba3abf0d19f14a61f20840cfe6dbe3a
Chris Burge <topher.burge@gmail.com>
2019-08-16 15:58:09 -0700

    add conditions to else if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,25 @@ $(document).ready(function(){
 
     var career = $("select#career").val();
     var company = $("select#company").val();
-    var important = $("select#important").val();
+    var interest = $("select#interest").val();
     var challenge = $("select#challenge").val();
     var animal = $("select#animal").val();
 
     $(".name").text(name);
 
-    if (career === "dev") {
+    if (career === "dev" && company === "google" || company === "hulu" && interest === "ai" || interest === "applications") {
         $("#python").show();
       }
-      else if (career === "web") {
+      else if (career === "software" && company === "google" || company === "hulu" && interest === "ai" || interest === "simulation") {
         $("#ruby").show();
       }
-      else if (career === "software") {
+      else if (career === "web" && company === "microsoft" || company === "google" && interest === "applications") {
         $("#c").show();
       }
-      else if (career === "hand") {
+      else if (career === "hand" && company === "pendant" && interest === "sitcom") {
         $("#george").show();
       }
-      else if (career === "none" || company === "none" || important === "none" || challenge === "none" || animal === "none") {
+      else if (animal === "none" || company === "none" || important === "none" || challenge === "none" || animal === "none") {
         alert("Oops, it looks like you forgot something!!")
       }
   });
